6c36ed022d72a81266b45dadb79e2b39fb7e6880
leirisson <103783610+leirisson@users.noreply.github.com>
2023-06-26 00:09:50 -0400

    REFGERG

---
+++ projeto_02/Linguagem-M/Estruturando-Tabela/e-comerce.m
@@ -0,0 +1,44 @@
+let
+       #"Conexão com a api" = // conectando com  a api
+            (numeroDePagina) => // função para deixar dinamico a numeração das paginas
+                    Json.Document( 
+                        Web.Contents("http://localhost:8080",
+                        [
+                            RelativePath = "/medicos",
+                            Query = [
+                                page = Number.ToText(numeroDePagina) //"1"//Number.ToText(numero_da_pagina) // convertendo para texto (OBS) o numero da pagina precisa ser em texto
+                            ]
+                        ])
+                    )[content],
+
+        #"Total de paginas" = 
+                    Json.Document( 
+                        Web.Contents("http://localhost:8080",
+                        [
+                            RelativePath = "/medicos",
+                            Query = [
+                                page = "1"
+                            ]
+                        ])
+                    )[totalPages],
+        #"Lista de paginas" = {0 .. #"Total de paginas" -1 },
+
+        #"Lista de todos os dados" = List.Transform(
+                            #"Lista de paginas",
+                            each {_, #"Conexão com a api"(_)} // para cada item da lista
+        ),
+        #"Table" = 
+        Table.ExpandRecordColumn(// expanção do registro da coluna -> Dados
+            Table.ExpandListColumn( // expandido a lista da coluna -> Dados 
+                                #table( // ------- tabela estruturada
+                                    type table
+                                    [Paginas = text, Dados=list], // registro da tabela
+                                    #"Lista de todos os dados"
+                                ),"Dados" // coluna que foi expandida
+                             ),
+                             "Dados", {"id","nome", "email", "crm", "especialidade"},
+                                      {"ID", "NOME", "E-MAIL", "CRM", "ESPECIALIDADE"} //renomeando ->  FORMATO QUE VAI SER EXIBIDO
+
+        )
+in
+   #"Table"
