011ad7ea291e0ca58889c880b96ca92fd0c40a18
leirisson <103783610+leirisson@users.noreply.github.com>
2023-06-26 00:13:53 -0400

    regrg

--- projeto_02/Linguagem-M/Estruturando-Tabela/e-comerce.m
+++ projeto_02/Linguagem-M/Estruturando-Tabela/e-comerce.m
@@ -27,18 +27,24 @@ let
                             #"Lista de paginas",
                             each {_, #"Conexão com a api"(_)} // para cada item da lista
         ),
-        #"Table" = 
-        Table.ExpandRecordColumn(// expanção do registro da coluna -> Dados
-            Table.ExpandListColumn( // expandido a lista da coluna -> Dados 
-                                #table( // ------- tabela estruturada
-                                    type table
-                                    [Paginas = text, Dados=list], // registro da tabela
-                                    #"Lista de todos os dados"
-                                ),"Dados" // coluna que foi expandida
-                             ),
-                             "Dados", {"id","nome", "email", "crm", "especialidade"},
-                                      {"ID", "NOME", "E-MAIL", "CRM", "ESPECIALIDADE"} //renomeando ->  FORMATO QUE VAI SER EXIBIDO
+        #"Table" =
+        Table.TransformColumns( 
+            Table.ExpandRecordColumn(// expanção do registro da coluna -> Dados
+                Table.ExpandListColumn( // expandido a lista da coluna -> Dados 
+                                    #table( // ------- tabela estruturada
+                                        type table
+                                        [Paginas = text, Dados=list], // registro da tabela
+                                        #"Lista de todos os dados"
+                                    ),"Dados" // coluna que foi expandida
+                                ),
+                                "Dados", {"id","nome", "email", "crm", "especialidade"},
+                                        {"ID", "NOME", "E-MAIL", "CRM", "ESPECIALIDADE"} //renomeando ->  FORMATO QUE VAI SER EXIBIDO
 
-        )
+            ),
+            {
+                "NOME",
+                each Text.FromBinary(Text.ToBinary(_, 1252), TextEncoding.Utf8)
+            }
+    ) // fim do  Table.TransformColumns
 in
    #"Table"
